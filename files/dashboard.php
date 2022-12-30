96b42d5eb6300f064b3ebf376e71959c4d8e865e
peter-geek <coderscave1@gmail.com>
2022-12-31 00:22:21 +0300

    Update. Reduced code redundancy. header.php, footer.php

--- files/dashboard.php
+++ files/dashboard.php
@@ -1,59 +1,20 @@
 <?php
-    //session_start();
-    if(isset($_GET['action']) && $_GET['action'] == 'logout')
-    unset($_SESSION['user']);
-
-    //echo '<pre>';
-    //print_r($_SESSION['user']);
-    //echo '</pre>';
-    ?>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<style>
-    .nav{
-        display: flex;
-        flex-direction: column;
-        width: 200px;
-        background-color: chocolate;
-        height: 100%;
-    }
-    .nav a{
-        color: #fff;
-        text-decoration: none;
-        padding: 10px 3px;
-    }
-    .nav > a:hover,
-    .nav > a.active{
-        background-color: #333;
-        color: #fff;
-    }
-    body{
-        display: flex;
-    }
-</style>
-<?php
-    if(isset($_GET['ref']))
-    $ref = $_GET['ref'];
-    else $ref = "home";
+if(isset($_GET['ref']))
+$ref = $_GET['ref'];
+else $ref = "home";
 ?>
-<body>
-    <nav class="nav">
-        <a href="?ref=home" class="<?php if($ref == 'home') echo "active" ?>">Home</a>
-        <a href="?ref=about" class="<?php if($ref == 'about') echo "active" ?>">Contact</a>
-        <a href="?ref=project" class="<?php if($ref == 'project') echo "active" ?>">Projects</a>
-        <a href="?action=logout" class="<?php if($action == 'about') echo "active" ?>">Logout</a>
-    </nav>
-    <div>
-        <?php
-            $file="files/". $ref.".php";
-            if(file_exists($file)) require "$file";
-        ?>
+<nav class="nav">
+    <div style="background-color: #420b34; color: #fff; padding: 10px;">
+        User: <?=$_SESSION['user']['name']?>
     </div>
-</body>
-</html>
+    <a href="?ref=home" class="<?php if($ref == 'home') echo "active" ?>">Home</a>
+    <a href="?ref=about" class="<?php if($ref == 'about') echo "active" ?>">Contact</a>
+    <a href="?ref=project" class="<?php if($ref == 'project') echo "active" ?>">Projects</a>
+    <a href="?logout">Logout</a>
+</nav>
+<div style="flex-grow: 1; height: 100%; overflow-y: scroll; overflow-x: hidden;">
+    <?php
+    $file="files/". $ref.".php";
+    if(file_exists($file)) require "$file";
+    ?>
+</div>
